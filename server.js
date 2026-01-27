73255e3fbef722d10c92ed23b0742d2001f42c3f
kirnsoraia <kirnsoraia@gmail.com>
2026-01-27 19:08:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ===== CONFIGURAÇÕES =====
-const FACEIT_API_KEY = "d7d99ce4-db81-49e0-904e-d05c7afd665a";
+const FACEIT_API_KEY = "86e45708-a971-41c8-b71c-8d88d0e1a98b";
 const FACEIT_NICK = "xsekh";
 const STEAM_ID_64 = "76561198953388206";
 
@@ -33,11 +33,18 @@ return {
 // ===== PREMIER =====
 async function getPremier() {
   const res = await fetch(
-    `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=730&steamid=${STEAM_ID_64}`
+    `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?key=${STEAM_API_KEY}&appid=730&steamid=${STEAM_ID_64}`
   );
+
+  if (!res.ok) {
+    const text = await res.text();
+    console.error("Steam API error:", text);
+    return "N/A";
+  }
+
   const data = await res.json();
 
-  const rating = data.playerstats.stats.find(
+  const rating = data.playerstats?.stats?.find(
     (s) => s.name === "cs2_premier_rating"
   )?.value;
 
